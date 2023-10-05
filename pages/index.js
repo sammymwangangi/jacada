f7edade56d47a8b5d598ccb1010a01800dd35723
sammymwangangi <sammymwinzi2@gmail.com>
2023-10-05 19:33:40 +0300

    changes

--- pages/index.js
+++ pages/index.js
@@ -225,58 +225,7 @@ const Home = ({ banner, brands, about_us, features, intro, speciality, testimoni
       </section>
 
       {/* About */}
-      <div className="section container">
-          <div className="row items-center justify-center">
-            <div className="animate md:col-6 lg:col-5 md:order-2">
-              <div className="about-image relative p-[60px]">
-                <ImageFallback
-                  className="animate relative w-full rounded-2xl"
-                  src={about_us.image}
-                  width={425}
-                  height={487}
-                  alt=""
-                />
-                <Circle
-                  className="top-4 left-4 z-[-1]"
-                  width={85}
-                  height={85}
-                />
-                <Circle
-                  width={37}
-                  height={37}
-                  fill={false}
-                  className="top-20 right-10 z-[-1]"
-                />
-                <Circle
-                  className="top-1/2 right-12 -z-[1]"
-                  width={24}
-                  height={24}
-                />
-                <Circle
-                  className="bottom-6 right-6 z-[-1]"
-                  width={85}
-                  height={85}
-                />
-                <Circle
-                  className="top-1/2 left-12 z-[-1]"
-                  width={20}
-                  height={20}
-                />
-                <Circle
-                  className="bottom-12 left-8 z-[1]"
-                  width={47}
-                  height={47}
-                  fill={false}
-                />
-              </div>
-            </div>
-            <div className="animate md:col-6 lg:col-4 md:order-1">
-              <p>{about_us.subtitle}</p>
-              {markdownify(about_us.title, "h2", "section-title bar-left mt-4")}
-              {markdownify(about_us.content, "p", "mt-10")}
-            </div>
-          </div>
-        </div>
+      
 
       {/* Features */}
       <section className="section">
